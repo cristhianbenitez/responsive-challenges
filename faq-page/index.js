d66fb4a0a1e56bbd1def73e2c880985c188103dd
Cristhian Benitez <cristhiantilleria@gmail.com>
2024-05-26 11:47:26 -0400

    fixing responsiveness

--- faq-page/index.js
+++ faq-page/index.js
@@ -2,3 +2,8 @@ $('.faq__question-list__item-accordion').click(function () {
   $(this).toggleClass('open');
   $(this).toggleClass('faq__question-list__item-accordion--open');
 });
+
+$('.faq__nav__list__item').click(function () {
+  $('.faq__nav__list__item').removeClass('faq__nav__list__item--active');
+  $(this).addClass('faq__nav__list__item--active');
+});
